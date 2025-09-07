0d60ae70bb095c322d045646192bc7b25174effb
SadmanSalim <sadmanmd225@gmail.com>
2025-09-07 19:58:58 +0600

    Refactor layout and enhance responsiveness by updating CSS styles, adding category slider, and improving HTML structure for better alignment and visibility.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,38 +10,59 @@ $(document).ready(function(){
         adaptiveHeight: true
     });
     $('.featured_slider').slick({
-        infinite: true,
-        slidesToShow: 4,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: true,
-        prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
-        nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
-        slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
+      infinite: true,
+      slidesToShow: 4,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+      nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+      slidesToScroll: 1,
+      responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                dots: false,
                 slidesToShow: 1,
-                slidesToScroll: 1
-              }
+                centerMode: true,
+                centerPadding: '35px'
             }
-          ]
-      });
+        }
+      ]
+  });
+  $('.category_slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+  
+  
                       
   });
