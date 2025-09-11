a02beca849efe340823f515abc8a5bf48cd33b35
SadmanSalim <sadmanmd225@gmail.com>
2025-09-11 20:48:54 +0600

    Add login, register, and menu pages; update navigation links and remove item counts from category slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
-    $('.banner_slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
-    $('.featured_slider').slick({
-      infinite: true,
-      slidesToShow: 4,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: true,
-      prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
-      nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
-      slidesToScroll: 1,
-      responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: true,
-                dots: false,
-                slidesToShow: 1,
-                centerMode: true,
-                centerPadding: '35px'
-            }
+$(document).ready(function () {
+  $('.banner_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+  $('.featured_slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          dots: false,
+          slidesToShow: 1,
+          centerMode: true,
+          centerPadding: '35px'
         }
-      ]
+      }
+    ]
   });
   $('.category_slider').slick({
     infinite: true,
@@ -37,32 +37,26 @@ $(document).ready(function(){
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows: true,
+    arrows: false,
     dots: false,
     prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
     nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
     responsive: [
       {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
+          centerMode: true,
+          centerPadding: '35px',
+          slidesToShow: 1,
         }
       }
     ]
   });
-  
-  
-                      
-  });
+
+
+
+
+
+
+
+});
